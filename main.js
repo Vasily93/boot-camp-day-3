701a4cac1f8b096643862f3556aebe516d078223
Vasily93 <markovavasily@gmail.com>
2019-01-16 20:42:49 -0800

    javascript

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
-var sum = 3+4
-console. log( 'hello console', sum);
+
+var x = 10;
+
+
+if (x > 12) {
+    console. log (x);
+
+    console. log('grooving with if')
+}
+
+if(x > 12) {
+   console. log('too small');
+}
+
+// 1. initialize
+// 2. comdition
+// 3. post loop action
+
+for(var i=0; i<x; i++) {
+   console. log(i)
+}
+
+if(i % 2===0) {
+   console.log(i);
+}
+
+function sayHello() {
+console.log('hi')
+}
+
+if(x % 2===0) {
+   sayHello()
+}
+
+function evenOrOdd() {
+
+
+
+   var input = document.getElementById('number')
+
+   var y=3;
+   if(y%2===0) {console.log('even');
+}
+
+else {
+   console.log('odd')
+
